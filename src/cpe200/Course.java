1265ea72d6ed74722790bd117eb72a0e4843a7db
Peerawit Naprae <peerawit503@gmail.com>
2016-09-09 14:59:52 +0700

    finished

--- src/cpe200/Course.java
+++ src/cpe200/Course.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 public class Course {
 
     public Course() {
-
         course_name = "TBA";
         course_id ="000000";
         lecturer = "TBA";
@@ -14,7 +13,6 @@ public class Course {
         no_students = 0;
 
     }
-
     public Course(String n, String cid) {
         this.course_name = !n.equalsIgnoreCase("") ? n : "TBA";
         this.course_id = !cid.equalsIgnoreCase("") ? cid : "000000";
@@ -30,7 +28,6 @@ public class Course {
         max_students = 30;
         no_students = 0;
     }
-
     public Course(String n, String cid, String l, int max) {
         this.course_name = !n.equalsIgnoreCase("") ? n : "TBA";
         this.course_id = !cid.equalsIgnoreCase("") ? cid : "000000";
@@ -38,7 +35,6 @@ public class Course {
         this.max_students = max > 9 ? max : 30;
         no_students = 0;
     }
-
     public String getCourse_name() {
         return course_name;
     }
@@ -46,60 +42,47 @@ public class Course {
     public void setCourse_name(String course_name) {
         this.course_name = !course_name.equalsIgnoreCase("")?course_name:this.course_name;
     }
-
     public String getCourse_id() {
         return course_id;
     }
-
     public void setCourse_id(String course_id) {
         this.course_id = isValidCourse_id(course_id)?course_id:this.course_id;
     }
-
     public String getLecturer() {
         return lecturer;
     }
-
     public void setLecturer(String l) {
         this.lecturer = !l.equalsIgnoreCase("") ? l : this.lecturer;
     }
-
     public int getMax_students() {
         return max_students;
     }
-
     public void setMax_students(int max) {
         this.max_students = max > 9 ? max : this.max_students;
     }
-
     public int getNo_students() {
-
         return no_students;
     }
-
     public void setNo_students(int no_students) {
         this.no_students = no_students >=0 && no_students < this.max_students?no_students:this.no_students;
     }
-
     @Override
     public String toString() {
         String o = this.course_name + " ("
                 + this.course_id + "), Teacher: "
                 + this.lecturer + ", has ";
 
+
         if(this.no_students < 2){
             return no_students==1?o+"ONE student, [maximum: "+this.max_students+"]":o+"NO student, [maximum: "+this.max_students+"]";
         }
-
         return o+this.no_students+" students, [maximum: "+this.max_students+"]";
-
     }
-
     private boolean isValidCourse_id(String id) {
         Pattern p = Pattern.compile(idREGEX);
         Matcher m = p.matcher(id);
         return m.matches()?true:false;
     }
-
     // Regular expression for the Student ID pattern
     private static final String idREGEX = "\\d{6}";
 
